c69d17a35aa3eb99ab341691a5667c1d87d0dd70
FelipeLopez <41812483+StereoSoda@users.noreply.github.com>
2025-02-25 17:16:53 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# PDFCombinerPY
-Combina PDF's: 2 en cada página tamaño carta
+# PDFCombinerPY from dflopez
+Toma PDF's y los combina
+
+# Instalación en Windows
+
+## Instalaciones requeridas
+
+```pip install PyPDF2 reportlab pdf2image Pillow colorama```	
+
+Descargar e instalar Poppler:
+ 
+-Ve a https://github.com/oschwartz10612/poppler-windows/releases.  
+-Descarga la última versión de poppler para Windows (por ejemplo, poppler-23.11.0.zip).  
+-Extrae el archivo ZIP en una carpeta, por ejemplo: C:\poppler.  
+-Agrega la carpeta bin de poppler a tu variable de entorno PATH:  
+-Haz clic derecho en "Este equipo" o "Mi PC" y selecciona "Propiedades".  
+-Haz clic en "Configuración avanzada del sistema".  
+-En la pestaña "Opciones avanzadas", haz clic en "Variables de entorno".  
+-En "Variables del sistema", busca la variable PATH y haz clic en "Editar".  
+-Agrega la ruta a la carpeta bin de poppler, por ejemplo: C:\poppler\bin.  
+-Haz clic en "Aceptar" para guardar los cambios.  
+
+# Instalación en MacOs
+
+## Instalaciones requeridas
+### En el path del script, ejecutar los siguientes comandos:
+```python3 -m venv venv``` (Creará el entorno virtual para Python).  
+```source venv/bin/activate``` (Activará el entorno virtual).  
+```pip install PyPDF2 reportlab pdf2image Pillow colorama``` (Instalará las librerías necesarias para la ejecución del script, también mencionadas en el archivo requirements).
+
+```deactivate``` (Para cerrar el entorno virtual, cuando se termine de utilizar el script).
+
+# Pasos para usar
+### Debe crear en el mismo path del script las carpetas "input" y "output".  
+-En input debe ir el/los PDF's a combinar.  
+-En output se generará el PDF combinado.  
+-```python combine_pdfs.py``` (En Windows para ejecutar el script).  
+-```python3 combine_pdfs.py``` (En MacOs con el entorno virtual activado para ejecutar el script).
+
+### Notas
+-El DPI se puede variar para la calidad requerida, todo depende de los recursos de la máquina. 
+
+
+![alt text](https://www.python.org/static/img/python-logo.png)
---
+++ combine_pdfs.py
@@ -0,0 +1,93 @@
+import os
+from PyPDF2 import PdfReader, PdfWriter
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.utils import ImageReader
+from reportlab.pdfgen import canvas
+from pdf2image import convert_from_path
+from io import BytesIO
+from PIL import Image
+from datetime import datetime
+from colorama import Fore, Back, Style, init
+
+# Inicializar colorama
+init(autoreset=True)
+
+# Desactivar el límite de seguridad de Pillow
+Image.MAX_IMAGE_PIXELS = None
+
+def combine_pdfs_from_directory(directory_path, output_path, dpi=300, batch_size=6):
+    pdf_files = [f for f in os.listdir(directory_path) if f.endswith(".pdf")]
+    pdf_files.sort()
+
+    if not pdf_files:
+        print(Back.RED + "No se encontraron archivos PDF en el directorio.")
+        return
+
+    writer = PdfWriter()
+
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d_%I-%M-%S%p").lower()
+    output_filename = f"output_{timestamp}.pdf"
+    output_path = os.path.join(output_path, output_filename)
+
+    total_pages = sum(len(convert_from_path(os.path.join(directory_path, pdf), dpi=dpi)) for pdf in pdf_files)
+    processed_tasks = 0
+
+    for pdf_file in pdf_files:
+        pdf_path = os.path.join(directory_path, pdf_file)
+        print(Back.YELLOW + f"Procesando: {pdf_file}")
+        images = convert_from_path(pdf_path, dpi=dpi, fmt="png", thread_count=4)
+        all_images = []
+
+        for image in images:
+            all_images.append(image)
+            if len(all_images) >= batch_size:
+                _process_images(all_images, writer, dpi)
+                all_images = []
+            processed_tasks += 1
+            print_progress_bar(processed_tasks, total_pages, prefix='Procesando', suffix=f'{processed_tasks}/{total_pages} Páginas')
+
+        if all_images:
+            _process_images(all_images, writer, dpi)
+
+    with open(output_path, "wb") as output_pdf:
+        writer.write(output_pdf)
+
+    print(Back.GREEN + f"\nSe combinó con éxito el PDF, por favor revisar en: {output_path}")
+
+def _process_images(images, writer, dpi):
+    for i in range(0, len(images), 2):
+        packet = BytesIO()
+        can = canvas.Canvas(packet, pagesize=letter)
+        img1 = images[i]
+        can.drawImage(ImageReader(img1), 0, 396, width=612, height=396, preserveAspectRatio=True)
+        if i + 1 < len(images):
+            img2 = images[i + 1]
+            can.drawImage(ImageReader(img2), 0, 0, width=612, height=396, preserveAspectRatio=True)
+        can.save()
+        packet.seek(0)
+        new_pdf = PdfReader(packet)
+        writer.add_page(new_pdf.pages[0])
+
+def print_progress_bar(iteration, total, prefix='', suffix='', length=50, fill='█', print_end="\r"):
+    gradient = [
+        Fore.YELLOW,
+        Fore.YELLOW,
+        Fore.GREEN,
+        Fore.GREEN,
+        Fore.LIGHTGREEN_EX
+    ]
+    percent = "{0:.1f}".format(100 * (iteration / float(total)))
+    filled_length = int(length * iteration // total)
+    bar = ''
+    for i in range(filled_length):
+        color = gradient[i * len(gradient) // length]
+        bar += f'{color}{fill}{Style.RESET_ALL}'
+    bar += '-' * (length - filled_length)
+    print(f'\r{prefix} |{bar}| {Fore.YELLOW}{percent}%{Style.RESET_ALL} {suffix}', end=print_end)
+    if iteration == total:
+        print()
+
+directory_path = "./input"
+output_path = "./output"
+combine_pdfs_from_directory(directory_path, output_path, dpi=300, batch_size=6)
